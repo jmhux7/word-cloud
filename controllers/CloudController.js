6a8dc8051a2b0f323b28ba8fff7efb470d31c090
Jonathan Huxhold <jhuxhold@gmail.com>
2016-12-05 16:55:27 -0700

    adding logic to adjust font sizes dynamically

--- controllers/CloudController.js
+++ controllers/CloudController.js
@@ -1,9 +1,8 @@
 app.controller('CloudController', function ($scope, WordService) {
-  var getWords = function() {
-    return {
-      currentWords: WordService.currentWords,
-      wordArr: WordService.wordArr[0]
-    }
+  $scope.currentWords = WordService.currentWords;
+  $scope.wordArr = WordService.wordArr[0];
+  $scope.getStyle = function(index){
+    var currentWord = WordService.currentWords[0][WordService.wordArr[0][index]];
+    return "font-size:" + (currentWord * 14) + "px"
   }
-  getWords();
 })
